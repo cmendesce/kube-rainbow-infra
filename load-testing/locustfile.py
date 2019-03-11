ab311877d0f8c9cec339aeec730866122c68e684
Carlos Mendes <cmendesce@gmail.com>
2019-03-11 06:48:39 -0300

    Fix infrastructure to test

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -9,12 +9,3 @@ class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
     min_wait = 5000
     max_wait = 15000
-
-
-    histogram_quantile(0.9, rate(caddy_http_request_duration_seconds_bucket{namespace="default"}[5m])) > 0.05
-      and
-    rate(caddy_http_request_duration_seconds_count{namespace="default"}[5m]) > 1
-
-    histogram_quantile(0.9, sum(rate(caddy_http_request_duration_seconds_bucket[5m])) by (job) ) / 1e+06
-
-,pod="znn-
